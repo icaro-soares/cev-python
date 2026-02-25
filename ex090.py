c385ac37ae4a9926878901c9e7c51b77bb0e2e4e
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-02-25 16:09:03 -0300

    Atualizei o repositório

--- ex090.py
+++ ex090.py
@@ -1,13 +1,3 @@
 """
 Faça um programa que leia o nome e a média de um aluno, guardando também a situação em um dicionário. No final mostre o conteúdo da estrutura na tela.
 """
-boletim = {}
-boletim['aluno'] = str(input('Nome: ')).strip()
-boletim['média'] = float(input('Média: '))
-if boletim['média'] >= 7.0:
-    boletim['situação'] = 'Aprovado(a)'
-elif 5.0 <= boletim['média'] <= 6.9:
-    boletim['situação'] = 'Recuperação'
-else:
-    boletim['situação'] = 'Reprovado(a)'
-print(boletim)
--- ex092.py
+++ ex092.py
@@ -2,21 +2,3 @@
 Crie um programa que leia o nome, ano de nascimento e carteira de trabalho e cadastre-os com idade em um dicionário, se a CTPS for diferente de zero. O dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 """
 #idade aposentar = 65
-
-from datetime import date
-
-dados = {}
-dados['nome'] = str(input('Nome Completo: ')).strip().title()
-nasc = int(input('Ano Nascimento: '))
-dados['idade'] = date.today().year - nasc
-dados['ctps'] = int(input('Nº da CTPS: '))
-if dados['ctps'] != 0:
-    dados['contratação'] = int(input('Ano de contratação: '))
-    dados['idade_contratação'] = dados['contratação'] - nasc
-    dados['salário'] = float(input('Salário: R$ '))
-    
-print('-='*30)
-for k, v in dados.items():
-    print(f'{k}: {v}')
-print('-='*30)
-
