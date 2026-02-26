310a92d5edfc333d4078f01579b1bb0b508ecdc4
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-02-26 09:25:08 -0300

    Atualizei o repositório

--- ex091.py
+++ ex091.py
@@ -2,7 +2,9 @@
 Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 """
 from random import randint
+from time import sleep
 
+c = 1
 dados = {
         'jogador1':randint(1, 6),
         'jogador2':randint(1, 6),
@@ -11,6 +13,13 @@ dados = {
 }
 for k, v in dados.items():
     print(f'O jogador {k} tirou {v} no dado')
+    sleep(1)
+dict_decrescente = dict(sorted(dados.items(), reverse=True))
 print('-='*30)
 print(f'{"ranking".upper():^60}')
 print('-='*30)
+
+for k, v in dict_decrescente.items():
+    print(f'{c}º Lugar {k} com {v}')
+    c+=1
+    
