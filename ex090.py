a2147da72301d5721ab5d3082dd9947d062c56a1
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-02-26 08:52:34 -0300

    Atualizei o repositório

--- ex090.py
+++ ex090.py
@@ -1,3 +1,15 @@
 """
 Faça um programa que leia o nome e a média de um aluno, guardando também a situação em um dicionário. No final mostre o conteúdo da estrutura na tela.
 """
+aluno = {}
+aluno['nome'] = str(input('Nome: ')).strip()
+aluno['média'] = float(input('Média: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado(a)'
+elif 5 <= aluno['média'] <= 6.9:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado(a)'
+print('-=')
+for k, v in aluno.items():
+    print(f'A chave: {k} tem valor: {v}')
