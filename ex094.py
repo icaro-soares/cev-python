f01d46c7ee6edb1b972098a3f883f2b93e4a19bd
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-02-27 09:17:48 -0300

    Atualizei o repositório

--- ex094.py
+++ ex094.py
@@ -5,3 +5,35 @@ b) A média de idade do grupo
 c) Uma lista com todas as mulheres
 d) Uma lista com todas as pessoas acima da média
 """
+ficha = {}
+lista = []
+lista_mulheres = []
+acima_média = []
+soma_idade = 0
+while True:
+    ficha['nome'] = str(input('Nome: ')).strip()
+    ficha['sexo'] = ' '
+    while ficha['sexo'] not in 'MmFf':
+        ficha['sexo'] = str(input('Sexo [M/F]: ')).strip()[0]
+    if ficha['sexo'] in 'Ff':
+        lista_mulheres.append(ficha['nome'])
+    ficha['idade'] = int(input('Idade: '))
+    soma_idade+=ficha['idade']
+    lista.append(ficha.copy())
+    r = ' '
+    while r not in 'SsNn':
+        r = str(input('Quer continuar [S/N]: ')).strip()[0]
+    if r in 'Nn':
+        break
+print('-='*30)
+print(f'Pessoas cadastradas: {len(lista)}')
+média_idade = soma_idade/len(lista)
+if ficha['idade'] > média_idade:
+    acima_média.append(ficha['nome'])
+print(f'Média de idade: {média_idade:.0f} anos')
+print(f'Lista de mulheres:')
+for m in lista_mulheres:
+    print(f'\t{m}')
+print(f'Lista de pessoas acima da média:')
+for p in acima_média:
+    print(f'\t{p}')
