1993c7491d9ceaaa8cb624b5d3519a1ff87ba391
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-04 08:45:05 -0300

    Atualizei o repositório

--- ex094.py
+++ ex094.py
@@ -8,26 +8,5 @@ d) Uma lista com todas as pessoas acima da média de idade
 ficha = {}
 pessoas = []
 mulheres = []
+acima_media = []
 soma_idade = 0
-while True:
-    ficha['nome'] = str(input('Nome: ')).strip()
-    ficha['sexo'] = ' '
-    while ficha['sexo'] not in 'MF':
-        ficha['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
-    if ficha['sexo'] == 'F':
-        mulheres.append(ficha['nome'])
-    ficha['idade'] = int(input('Idade: '))
-    soma_idade+=ficha['idade']
-    pessoas.append(ficha.copy())
-    resp = ' '
-    while resp not in 'SN':
-        resp = str(input('Quer continuar [S/N]: ')).strip().upper()[0]
-    if resp == 'N':
-        break
-print('-='*30)
-print(f'Foram cadastradas {len(pessoas)} pessoas')
-media_idade = soma_idade/len(pessoas)
-print(f'A média de idade do grupo é {media_idade:.0f}')
-print('Lista de mulheres')
-for m in mulheres:
-    print(f'\t{m}')
