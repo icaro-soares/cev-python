95f9854a456ea1f71f09070b7791c4e9bc35c45e
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-05 09:34:40 -0300

    Atualizei o jogo adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,13 +3,16 @@ from random import randint
 from time import sleep
 
 while True:
+
     número_secreto = randint(0, 100)
     tentativas = 0
     pontos = 1000 #pontuação será subtração da diferença entre o chute e o número secreto
     acertou = False
+
     print('-='*30)
     print(f'{"Jogo da adivinhação":^60}')
     print('-='*30)
+
     dificuldade = int(input('Escolha a dificuldade [1 - Fácil][2 - Médio][3 - Difícil]: '))
     match dificuldade:
         case 1:
@@ -24,10 +27,13 @@ while True:
         case _:
             print('Comando desconhecido, reinicie o programa e tente novamente!')
             exit()
+
     while not acertou:
+
         chute = int(input('Diga seu chute: '))
         tentativas+=1
         chances-=1
+
         if chute == número_secreto:
             print('Você acertou!')
             print(f'Número de tentativas: {tentativas}')
@@ -40,11 +46,11 @@ while True:
                 pontos-=(chute - número_secreto)
                 print('Menos...')
             if pontos <= 0 or chances == 0:
-                print(f'Você perdeu! Sua pontuação foi zerada! O número é: {número_secreto}')
+                print(f'Você perdeu, a sua pontuação foi zerada!\nO número secreto é: {número_secreto}!')
                 pontos = 0
                 break
     print(f'Pontuação final: {pontos}')
-    print('-='*60)
+
     resp = ' '
     while resp not in 'SsNn':
         resp = str(input('Quer jogar de novo [S/N]? ')).strip()[0]
@@ -54,4 +60,3 @@ while True:
         break
     elif resp in 'Ss':
         os.system('cls' if os.name == 'nt' else 'clear')
-        
