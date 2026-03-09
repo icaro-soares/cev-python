a89253bd3809a70c0490f2644dc71971c231fddc
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-09 16:51:08 -0300

    Atualizei o ex092.py

--- ex092.py
+++ ex092.py
@@ -2,3 +2,17 @@
 Crie um programa que leia o nome, ano de nascimento e carteira de trabalho e cadastre-os com idade em um dicionário. Se a CTPS for diferente de zero, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 """
 #idade aposentar = 65
+from datetime import date
+
+ficha = {}
+ficha['nome'] = str(input('Nome: ')).strip()
+nasc = int(input('Ano de nascimento: '))
+ficha['idade'] = date.today().year - nasc
+ficha['ctps'] = int(input('CTPS: '))
+if ficha['ctps'] != 0:
+    ficha['contratação'] = int(input('Ano de contratação: '))
+    ficha['salário'] = float(input('Salário: R$ '))
+    ficha['aposentadoria'] = ficha['idade'] + ((ficha['contratação'] + 65) - date.today().year)
+print('-='*30)
+for k, v in ficha.items():
+    print(f'\t- {k} tem valor {v}')
