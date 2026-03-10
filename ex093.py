b1e7f418d095fefd31772aba76f3ccf823950824
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-10 09:05:48 -0300

    Atualizei o ex093

--- ex093.py
+++ ex093.py
@@ -7,7 +7,7 @@ jogador['nome'] = str(input('Nome do jogador: ')).strip()
 jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0, jogador['partidas']):
     gols.append(int(input(f'Gols na partida {c+1}: ')))
-jogador['gols'] = gols
+jogador['gols'] = gols[:]
 jogador['total'] = sum(gols)
 print('-='*30)
 print(jogador)
