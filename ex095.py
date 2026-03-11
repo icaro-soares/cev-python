69663e1a07f9d4ce1e8905fddd6dd2064be8b11c
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 14:37:33 -0300

    Atualizei o ex095

--- ex095.py
+++ ex095.py
@@ -1,21 +1,20 @@
 """
 Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
+time = []
 jogador = {}
 partidas = []
-time = []
 
 while True:
-
     jogador.clear()
     jogador['nome'] = str(input('Nome: '))
     tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
     for c in range(0, tot):
         partidas.append(int(input(f'Gols na partida {c+1}: ')))
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
     time.append(jogador.copy())
-
     while True:
         resp = str(input('Quer continuar [S/N]? ')).strip()[0]
         if resp in 'SsNn':
@@ -23,5 +22,15 @@ while True:
         print('Erro! Apenas S ou N!')
     if resp in 'Nn':
         break
-
-print(time)
+print('-='*30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print(f'-='*40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print(f'-='*40)
