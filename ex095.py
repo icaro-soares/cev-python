eb650f5b2d12bdc0a46e3b7baf144b6a711a3c15
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 14:10:13 -0300

    Atualizei o ex095

--- ex095.py
+++ ex095.py
@@ -3,12 +3,23 @@ Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um
 """
 jogador = {}
 gol = []
-jogador['nome'] = str(input('Nome: '))
-part = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-for c in range(0, part):
-    gol.append(int(input(f'Gols na partida {c+1}: ')))
-jogador['gols'] = gol[:]
-jogador['total'] = sum(gol)
+
+while True:
+    jogador['nome'] = str(input('Nome: '))
+    part = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    for c in range(0, part):
+        gol.append(int(input(f'Gols na partida {c+1}: ')))
+    jogador['gols'] = gol[:]
+    jogador['total'] = sum(gol)
+
+    while True:
+        resp = str(input('Quer continuar [S/N]? ')).strip()[0]
+        if resp in 'SsNn':
+            break
+        print('Erro! Apenas S ou N!')
+    if resp in 'Nn':
+        break
+
 print(jogador)
 print('-='*30)
 for k, v in jogador.items():
