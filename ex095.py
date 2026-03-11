e7b0e448e33f574cd234a9ab76f631da8de20dd0
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 14:05:26 -0300

    Atualizei o ex095

--- ex095.py
+++ ex095.py
@@ -2,21 +2,19 @@
 Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
 jogador = {}
-gols = []
-while True:
-    jogador.clear()
-    jogador['nome'] = str(input('Nome do jogador: '))
-    jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
-    for c in range(0, jogador['partidas']):
-        gols.append(int(input(f'Quantos gols na partida {c+1}? ')))
-    jogador['gols'] = gols[:]
-    jogador['total'] = sum(gols)
-    while True:
-        resp = str(input('Quer continuar [S/N]: '))[0]
-        if resp in 'SsNn':
-            break
-        print('ERRO! Apenas S ou N')
-    if resp in 'Nn':
-        break
-print('-='*30)
+gol = []
+jogador['nome'] = str(input('Nome: '))
+part = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, part):
+    gol.append(int(input(f'Gols na partida {c+1}: ')))
+jogador['gols'] = gol[:]
+jogador['total'] = sum(gol)
 print(jogador)
+print('-='*30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem valor {v}')
+print('-='*30)
+print(f'O jogador {jogador["nome"]} jogou {part} partidas.')
+for k, v in enumerate(gol):
+    print(f'\tNa partida {k+1} fez {v} gols.')
+print('-='*30)
