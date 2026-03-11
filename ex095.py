340f9c2e9be8a1434bfe24e733f806620e338aeb
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 09:10:32 -0300

    Atualizei o repositório

--- ex095.py
+++ ex095.py
@@ -1,3 +1,22 @@
 """
 Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
+jogador = {}
+gols = []
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    jogador['partidas'] = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    for c in range(0, jogador['partidas']):
+        gols.append(int(input(f'Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = gols[:]
+    jogador['total'] = sum(gols)
+    while True:
+        resp = str(input('Quer continuar [S/N]: '))[0]
+        if resp in 'SsNn':
+            break
+        print('ERRO! Apenas S ou N')
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(jogador)
