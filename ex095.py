245c869f0710c1e43f7274ffbc49619b27f9b3c5
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 14:49:31 -0300

    Atualizei o ex95

--- ex095.py
+++ ex095.py
@@ -34,3 +34,15 @@ for k, v in enumerate(time):
         print(f'{str(d):<15}', end='')
     print()
 print(f'-='*40)
+while True:
+    busca = int(input('Mostrar dados de que jogador? '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print('Erro! Não existe o jogador com esse código!')
+    else:
+        print(f' --- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
+        for i, g in enumerate(time[busca]["gols"]):
+            print(f'\tNo jogo {i+1} fez {g} gols.')
+    print('-='*40)
+print(f'<< Volte sempre >>')
