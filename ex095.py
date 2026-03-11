1085e311d71172aee3bd6122b65ad9c5dee4ba6e
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-11 14:15:59 -0300

    Atualizei o ex095

--- ex095.py
+++ ex095.py
@@ -3,14 +3,18 @@ Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um
 """
 jogador = {}
 gol = []
+time = []
 
 while True:
+
+    jogador.clear()
     jogador['nome'] = str(input('Nome: '))
     part = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
     for c in range(0, part):
         gol.append(int(input(f'Gols na partida {c+1}: ')))
     jogador['gols'] = gol[:]
     jogador['total'] = sum(gol)
+    time.append(jogador.copy())
 
     while True:
         resp = str(input('Quer continuar [S/N]? ')).strip()[0]
@@ -20,12 +24,4 @@ while True:
     if resp in 'Nn':
         break
 
-print(jogador)
-print('-='*30)
-for k, v in jogador.items():
-    print(f'O campo {k} tem valor {v}')
-print('-='*30)
-print(f'O jogador {jogador["nome"]} jogou {part} partidas.')
-for k, v in enumerate(gol):
-    print(f'\tNa partida {k+1} fez {v} gols.')
-print('-='*30)
+print(time)
