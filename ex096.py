39ba997b3070e12c949f3484fd873fabfcf4e731
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-16 14:42:55 -0300

    Atualizei o repositório

---
+++ ex096.py
@@ -0,0 +1,3 @@
+"""
+Faça um exercício que tenha uma função chamada area, que receba duas dimensões de um terreno retângular (largura e comprimento) e mostre a área do terreno.
+"""
---
+++ ex099.py
@@ -0,0 +1,4 @@
+"""
+Faça um programa que tenha uma função chamada maior(), que receba vários valores inteiros com parâmetro.
+Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+"""
