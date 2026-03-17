ed0d4fa690080a200c6e266a96941cf71bb530e9
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-17 09:28:30 -0300

    Atualizei o repositório

--- ex099.py
+++ ex099.py
@@ -5,15 +5,10 @@ Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 
 
 def maior(*num):
-    print('Analisando os valores passados...')
-    print('Foram passados os valores ', end='')
-    for n in num:
-        print(f'{n} ', end='')
-    print(f'\nO maior valor é {max(num)}')
-    print('-='*20)
-
+    
 
 maior(7, 5, 3)
-maior(4, 10, 14, 2, 0, 10)
-maior(1, 3, 0, 1, 2)
-maior(7, 6, 1, 9)
+#maior(4, 10, 14, 2, 0, 10)
+#maior(1, 3, 0, 1, 2)
+#maior(7, 6, 1, 9)
+#maior()
