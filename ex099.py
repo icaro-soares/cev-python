bda3cf8a1cd5c87eb868dfb7207737cd5a085206
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-17 08:22:10 -0300

    Atualizei o ex099

--- ex099.py
+++ ex099.py
@@ -2,3 +2,13 @@
 Faça um programa que tenha uma função chamada maior(), que receba vários valores inteiros com parâmetro.
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 """
+
+
+def maior(*num):
+    print(f'O maior valor é {max(num)}')
+
+
+maior(7, 5, 3)
+maior(4, 10, 14, 2, 0, 10)
+maior(1, 3, 0, 1, 2)
+maior(7, 6, 1, 9)
