71563069dbc89fa14efefc0c2eba8ebd22479e1b
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-17 08:05:15 -0300

    Atualizei o ex097

--- ex097.py
+++ ex097.py
@@ -1,3 +1,15 @@
 """
 Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável
 """
+
+
+def escreva(msg):
+    print('~'*len(msg))
+    print(msg)
+    print('~'*len(msg))
+
+
+
+escreva(' Curso em vídeo ')
+escreva(' Python é Legal Demais ')
+escreva(' Estudar Python Muda Vidas ')
