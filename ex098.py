baec0b082aca57976efd95a866dfb4481ef8c699
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-17 08:12:13 -0300

    Atualizei o ex098

--- ex098.py
+++ ex098.py
@@ -4,3 +4,20 @@ a) de 1 até 10 de 1 em 1
 b) de 10 até 0 de 2 em 2
 c) Uma contagem personalizada
 """
+
+
+def contador(i=1, f=10, p=1):
+    if i < f:
+        while i <= f:
+            print(f'{i} ', end='')
+            i+=p
+        print('Fim!')
+    elif i > f:
+        while i >= f:
+            print(f'{i} ', end='')
+            i-=p
+        print('Fim!')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
