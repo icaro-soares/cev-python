4c7aaaad1cf4e16c8d9d26ffc6a7feb12048db4a
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-17 08:30:11 -0300

    Atualizei o ex100

--- ex100.py
+++ ex100.py
@@ -1,3 +1,16 @@
 """
 Faça um programa que tenha uma lista chamada números e duas funções chamada sorteia() e somarPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 """
+from random import randint
+
+
+def sorteia(lista):
+    cont = 0
+    while cont <= 4:
+        lista.append(randint(0, 30))
+        cont+=1
+    print(f'Seus números foram {lista}')
+
+
+números = []
+sorteia(números)
