3d70109d94e0a186ef5918ae16589a8250b56993
Ícaro Soares <151578696+icaro-soares@users.noreply.github.com>
2026-03-20 10:35:47 -0300

    Atualizei o repositório

---
+++ ex104.py
@@ -0,0 +1,3 @@
+"""
+Crie um programa que tenha a função leiaint(), que vai funcionar de forma semelhante ao input(), só que fazendo a validação para aceitar apenas um valor numérico.
+"""
---
+++ ex106.py
@@ -0,0 +1,4 @@
+"""
+Faça um mini-sitema que utilize o Interactive Help do python. O usuário vai digitar o comando e o manual vai aparecer. Quanto o usuário digitar a palavra FIM o programa se encerrará.
+OBS.: Use cores.
+"""
